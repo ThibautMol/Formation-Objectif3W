9b1fcee6f04942a70ec87f8927062bcce606dc7b
Thibaut Molinié <123956618+ThibautMol@users.noreply.github.com>
2023-05-04 16:58:21 +0200

    update

--- PHP/condition.php
+++ PHP/condition.php
@@ -1,3 +1,71 @@
 <?php
 
-echo 'saut du dossier cour';
+// == : permet de tester l'égalité (à pas confondre avec le = simple qui attribu une valeur)
+// != : permet de tester l'inégalité
+// < : strictement inférieur
+// > : strictement supérieur
+// => : supérieur ou égal
+// <= : inférieur ou égal
+// && (AND) : l'opérateur ET, permet de préciser une condition en association
+// || (OR) : l'opérateur OU
+// === : permet de tester l'égalité sur un même type de donnée (int, float, string) mais aussi si les éléments sont égaux
+
+// ET
+// VRAI && VRAI = VRAI
+// FAUX && VRAI = FAUX
+// FAUX && FAUX = FAUX
+
+// OU
+// VRAI || VRAI = VRAI
+// FAUX || VRAI = VRAI
+// FAUX || FAUX = FAUX
+
+
+// if (condition){
+//     //instruction si la condition est vérifiée
+// }
+// Else {
+    // Instruction si la condition n'est pas vérifiée
+// }
+
+$age=19;
+if ($age >18 && $age<35);
+
+
+
+
+$age = (int)readline ('saisissez votre age : ')
+
+if ($age <18) {
+    echo 'Vous êtes trop jeune pour rentrer' . PHP_EOL;
+}
+
+else {
+    echo 'vous pouvez rentrer' . PHP_EOL;
+}
+
+$temps = 'pluvieux';
+if ($temps==='ensoleillé') {
+    echo 'il fait beau' . PHP_EOL;
+}
+else {
+    echo 'il fait pas beau' . PHP_EOL;
+}
+
+$roleUser= 'gamer';
+
+if ($roleUser==='admin') {
+    echo 'accès à l\'espace admin' . PHP_EOL;
+}
+
+else {
+    echo 'renvoie sur la page de connexion' . PHP_EOL;
+}
+
+// condition?'verifiée:'pas verifié'=ternaire
+
+$temps = 'pluvieux';
+echo $temps=== 'ensoleillé' ? 'il fait beau' : 'il fait pas beau' . PHP_EOL;
+
+$roleUser= 'gamer';
+echo $roleUser === 'admin' ? 'accès a l\'espace admin' : 'renvoie sur la page connexion' . PHP_EOL;

96220f9468f1fd32bce0bb66663efa363fd37865
Thibaut Molinié <123956618+ThibautMol@users.noreply.github.com>
2023-05-05 15:45:22 +0200

    update

--- PHP/TP/while.php
+++ PHP/TP/while.php
@@ -4,14 +4,14 @@
 // et affiché le nombre de saisie total ainsi que la somme des valeurs saisies
 
 $valeur=0;
-$compteur=0;
+$compteur=-1;
 $total=0;
 
 while ($valeur!=-1) {
     $valeur=(float)readline('Entrez votre valeur : ');
-    $compteur=($compteur+1);
+    $compteur++;
     $total=($total+$valeur);
 }
 
-echo 'Le nombre de saisie était de ' . ($compteur-1) . PHP_EOL;
+echo 'Le nombre de saisie était de ' . ($compteur) . PHP_EOL;
 echo 'La somme des valeurs saisies est de ' . ($total+1) . PHP_EOL;

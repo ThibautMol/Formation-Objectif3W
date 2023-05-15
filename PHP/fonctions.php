3f244d6dd1fbe74ccaa86b9e6d2c1cb1cd788204
Thibaut Molinié <123956618+ThibautMol@users.noreply.github.com>
2023-05-15 13:54:49 +0200

    update

--- PHP/fonctions.php
+++ PHP/fonctions.php
@@ -1,2 +1,17 @@
 <?php
 
+// les arguments ou parametres sont des aleurs que l'on va fournir à la fonction
+
+// function nomFonction1 ($argument1,$argument2) {
+//     // code de la fonction
+// }
+
+// function nomFonction2 ($parametre1,$parametre2) {
+//     // code de la fonction
+// }
+
+function direBonjour($prenom){
+    echo "Bonjour $prenom";
+}
+
+direBonjour('Sony');

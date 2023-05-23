2e4b3263083f029f729a91853f849117bf404849
Thibaut Molinié <123956618+ThibautMol@users.noreply.github.com>
2023-05-23 14:58:21 +0200

    update

--- Projet de groupe/Code/footer.php
+++ Projet de groupe/Code/footer.php
@@ -3,7 +3,7 @@
         <footer class="bg-dark text-center text-white">
             <!-- Copyright -->
             <div class="text-center p-3" style="">
-                © 2022 Copyright:
+                © 2023 Copyright:
                 <a class="text-white" href="https://objectif3w.com/">Objectif 3W</a>
             </div>
             <!-- Copyright -->

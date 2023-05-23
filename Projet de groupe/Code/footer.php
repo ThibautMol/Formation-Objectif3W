5f93d898f9b10e18dba48defe3553bbc6cce5ae5
Thibaut Molinié <123956618+ThibautMol@users.noreply.github.com>
2023-05-23 14:49:22 +0200

    update

--- Projet de groupe/Code/footer.php
+++ Projet de groupe/Code/footer.php
@@ -3,8 +3,8 @@
         <footer class="bg-dark text-center text-white">
             <!-- Copyright -->
             <div class="text-center p-3" style="">
-                © 2020 Copyright:
-                <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+                © 2022 Copyright:
+                <a class="text-white" href="https://objectif3w.com/">Objectif 3W</a>
             </div>
             <!-- Copyright -->
         </footer>

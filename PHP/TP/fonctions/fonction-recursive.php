33ec5e321fba43c00a25f1dae430f47cc781efc6
Thibaut Molinié <123956618+ThibautMol@users.noreply.github.com>
2023-05-26 16:42:35 +0200

    update

--- PHP/TP/fonctions/fonction-recursive.php
+++ PHP/TP/fonctions/fonction-recursive.php
@@ -1,5 +1,24 @@
 <?php
 
 function decrement($num) {
-    
+    if ($num === 0) {
+        echo 'Terminé';
+    }else {
+        echo $num . ', ';
+        decrement($num-1);
+    }
 }
+
+// decrement (6);
+
+function factorielle(int $n) : int{
+    if($n===0) {
+        $resultat=1;
+    }else{
+        $resultat = factorielle ($n-1)*$n;
+        echo $resultat . PHP_EOL;
+    }
+    return $resultat;
+}
+
+echo factorielle(5);

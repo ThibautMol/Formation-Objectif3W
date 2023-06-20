2af3d0741f73112e3c2f7f520881de43b550dce2
Thibaut Molinié <123956618+ThibautMol@users.noreply.github.com>
2023-06-20 13:40:17 +0200

    new exercices done

---
+++ PHP/TP/sessions/mini-jeu/function-mini-game.php
@@ -0,0 +1,52 @@
+<?php
+  session_start();
+
+  
+
+
+if (!isset($_SESSION['guessing_game']['player_number'])) {
+
+    $_SESSION['guessing_game']['number']=rand(1,15);
+
+
+}
+
+
+function guessing_number_game ($guess) {
+
+    settype($guess,"integer");
+    $number_to_guess=$_SESSION['guessing_game']['number'];
+    settype($number_to_guess,"integer");
+
+    unset($_SESSION['guessing_game']['error']);
+   
+
+    if ($guess==$number_to_guess) {
+        return "C'est gagné";
+    }
+    elseif ($guess>$number_to_guess) {
+        return "Nombre trop grand";
+    }
+    else {
+        return "Nombre trop petit";
+    }
+
+}
+
+
+if (isset($_POST['player_number']) && !empty($_POST['player_number'])) {
+    if (is_numeric($_POST['player_number'])) {
+
+        $guess=$_POST['player_number'];
+        $_SESSION['guessing_game']['player_number'][]=$guess;
+
+        $_SESSION['guessing_game']['result']=guessing_number_game($guess);
+    }
+    else {
+        $_SESSION['guessing_game']['error']="Merci de rentrer un chiffre";
+    }
+   
+}
+
+header('Location: http://localhost/Formation-Objectif3W/PHP/TP/sessions/mini-jeu/mini-jeu.php');
+exit;

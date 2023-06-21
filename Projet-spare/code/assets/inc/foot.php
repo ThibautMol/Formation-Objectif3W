8b42efe8b7d4765ce8f5560d2ea5e254d42b10e4
Thibaut Molinié <123956618+ThibautMol@users.noreply.github.com>
2023-06-21 17:01:30 +0200

    update

--- Projet-spare/code/assets/inc/foot.php
+++ Projet-spare/code/assets/inc/foot.php
@@ -1,6 +1,6 @@
 <footer class="bg-dark text-center text-white fixed-bottom">
             <!-- Copyright -->
-            <div class="text-center p-3" style="">
+            <div class="text-center p-3">
                 © 2023 Copyright:
                 <a class="text-white" target="_blank" href="https://objectif3w.com/">Objectif 3W</a>
             </div>

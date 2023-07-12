48e07a4f192f3de1b9a9c6b5002e747482a215d8
Thibaut Molinié <123956618+ThibautMol@users.noreply.github.com>
2023-07-12 16:15:01 +0200

    all working withou comment

--- Portfolio/pendu/interface-pendu.php
+++ Portfolio/pendu/interface-pendu.php
@@ -8,20 +8,17 @@ require_once ('../assets/inc/navbar.php');?>
 
 
 
-<section class="d-flex flex-column justify-content-center mt-5 container-sm">
+    <section class="d-flex flex-column justify-content-center mt-5 container-sm">
 
         <h1 class="text-center">Jeu du pendu</h1>
 
         <p class="text-center">Devinez un mot</p>
-
-        <pre><?=var_dump($_SESSION)?></pre>
-
-        <p>
-            ajouter images + faire marcher bouton refaire une partie
-        </p>
+        <div class="w-25 mx-auto <?=(isset($_SESSION['pendu']['game']['tentatives'])) ? "" : "d-none" ?>">
+            <img  title="pendu-<?=(isset($_SESSION['pendu']['game']['tentatives'])) ? $_SESSION['pendu']['game']['tentatives'] : ""?>-try" src="<?=(isset($_SESSION['pendu']['game']['tentatives'])) ? "../assets/img/pendu/pendu-" . $_SESSION['pendu']['game']['tentatives'] . "-try.png" : "..." ?>" alt="pendu-<?=(isset($_SESSION['pendu']['game']['tentatives'])) ? $_SESSION['pendu']['game']['tentatives'] : ""?>-try" >
+        </div>
 
         <div class="d-flex my-2 justify-content-evenly">
-            <button class="btn btn-primary <?=(!isset($_SESSION['pendu']['game']['result'])) ? "d-none" : ""?>">
+            <button class="btn btn-primary <?=(isset($_SESSION['pendu']['game']['result'])) ? "" : "d-none"?>">
                 <a class="nav-link text-white" href="<?=(!isset($_SESSION['pendu']['game']['result'])) ? "" : "../assets/session-killer.php"?>">Relancer une partie</a>
             </button>
         </div>
@@ -31,12 +28,12 @@ require_once ('../assets/inc/navbar.php');?>
             <div class="d-flex">
                 <p class="me-1">Le mot était :</p>
                 <p class="text-uppercase">
+
                     <?php if (($_SESSION['pendu']['game']['result'])):?>
                         <?php foreach ($_SESSION['pendu']['game']['word_to_guess_split'] as $letter):?>
                             <?=$letter?>
                         <?php endforeach?>
                     <?php endif?>
-                    
                     
                 </p>
             </div>
@@ -85,68 +82,72 @@ require_once ('../assets/inc/navbar.php');?>
             <?php endif?>
         </div> 
     
+        <div class="d-flex justify-content-center">
+            <form class="<?=((!isset($_SESSION['pendu']['game']['launch_game'])) && 
+                (empty($_SESSION['pendu']['game']['launch_game'])) && 
+                (!isset($_SESSION['pendu']['game']['result'])) && 
+                (empty($_SESSION['pendu']['game']['result']))) ? "d-none" : ""?>" action="<?=((!isset($_SESSION['pendu']['game']['launch_game'])) && 
+                (empty($_SESSION['pendu']['game']['launch_game'])) && 
+                (!isset($_SESSION['pendu']['game']['result'])) && 
+                (empty($_SESSION['pendu']['game']['result']))) ? "" : "traitement-pendu.php"?>" method="POST">
 
-        <form class="<?=((!isset($_SESSION['pendu']['game']['launch_game'])) && 
-            (empty($_SESSION['pendu']['game']['launch_game'])) && 
-            (!isset($_SESSION['pendu']['game']['result'])) && 
-            (empty($_SESSION['pendu']['game']['result']))) ? "d-none" : ""?>" action="<?=((!isset($_SESSION['pendu']['game']['launch_game'])) && 
-            (empty($_SESSION['pendu']['game']['launch_game'])) && 
-            (!isset($_SESSION['pendu']['game']['result'])) && 
-            (empty($_SESSION['pendu']['game']['result']))) ? "" : "traitement-pendu.php"?>" method="POST">
+                <div class="mx-auto w-25 <?=((!isset($_SESSION['pendu']['game']['result'])) && (empty($_SESSION['pendu']['game']['result']))) ? "" : "d-none"?>">
+                    <label class="text-capitalize" for="player_letter">Lettre</label>
+                    <input type="text" class="form-control" name="player_letter" id="player_letter" minlength="1" maxlength="1" required> 
+                    <button class="btn btn-primary my-2" type="submit">Valider</button>
+                </div>
 
-            <div class="mx-auto w-25 <?=((!isset($_SESSION['pendu']['game']['result'])) && (empty($_SESSION['pendu']['game']['result']))) ? "" : "d-none"?>">
-                <label class="text-capitalize" for="player_number">Lettre</label>
-                <input type="text" class="form-control" name="player_letter" id="player_letter" minlength="1" maxlength="1" required> 
-                <button class="btn btn-primary my-2" type="submit">Valider</button>
-            </div>
+            </form>
 
-        </form>
+            <form class="<?=((!isset($_SESSION['pendu']['game']['launch_game'])) && 
+                (empty($_SESSION['pendu']['game']['launch_game'])) && 
+                (!isset($_SESSION['pendu']['game']['result'])) && 
+                (empty($_SESSION['pendu']['game']['result']))) ? "d-none" : ""?>" action="<?=((!isset($_SESSION['pendu']['game']['launch_game'])) && 
+                (empty($_SESSION['pendu']['game']['launch_game'])) && 
+                (!isset($_SESSION['pendu']['game']['result'])) && 
+                (empty($_SESSION['pendu']['game']['result']))) ? "" : "traitement-pendu.php"?>" method="POST" >
 
-        <form class="<?=((!isset($_SESSION['pendu']['game']['launch_game'])) && 
-            (empty($_SESSION['pendu']['game']['launch_game'])) && 
-            (!isset($_SESSION['pendu']['game']['result'])) && 
-            (empty($_SESSION['pendu']['game']['result']))) ? "d-none" : ""?>" action="<?=((!isset($_SESSION['pendu']['game']['launch_game'])) && 
-            (empty($_SESSION['pendu']['game']['launch_game'])) && 
-            (!isset($_SESSION['pendu']['game']['result'])) && 
-            (empty($_SESSION['pendu']['game']['result']))) ? "" : "traitement-pendu.php"?>" method="POST">
-
-            <div class="mx-auto w-25 <?=((!isset($_SESSION['pendu']['game']['result'])) && (empty($_SESSION['pendu']['game']['result'])) && ($_SESSION['pendu']['game']['tentatives']>3)) ? "" : "d-none"?>">
-                <input type="text" class="form-control" name="player_word" id="player_word" minlength="3" placeholder="Proposez votre mot de plus de 2 charactères" required> 
-                <button class="btn btn-primary my-2" type="submit">Valider</button>
-            </div>
-            
-        </form>
-
-        <div class=" d-flex flex-column justify-content-center <?=((isset($_SESSION['pendu']['game']['tentatives']))) ? "" : "d-none" ?>">
-            <h4 class="mx-auto">Lettres proposées</h4>
-            <div class="d-flex justify-content-center my-3 gap-3">
-            
-                <?php if ((isset($_SESSION['pendu']['game']['all_letter_proposed_without_spe_char'])) && (!empty($_SESSION['pendu']['game']['all_letter_proposed_without_spe_char'])))  :?>
-                    <?php foreach ($_SESSION['pendu']['game']['all_letter_proposed_without_spe_char'] as $key => $value) :?> 
-                    
-                        <div class="text-capitalize border rounded border-dark rounded text-center <?=(in_array($value,$_SESSION['pendu']['game']['word_split_without_spe_char'])) ? "bg-success" . " " . "text-white" : ""?>" style="width:20px">
-                            <?=$value?>
-                        </div>
-                    
-                    <?php endforeach?>
-                <?php endif?>
-            </div>
+                <div class="mx-auto w-25 <?=((!isset($_SESSION['pendu']['game']['result'])) && (empty($_SESSION['pendu']['game']['result'])) && ($_SESSION['pendu']['game']['tentatives']>3)) ? "" : "d-none"?>">
+                    <label class="text-capitalize" for="player_word">Mot</label>
+                    <input type="text" class="form-control" name="player_word" id="player_word" minlength="3" placeholder="Mot de plus de 2 lettres" style="width:15rem"required> 
+                    <button class="btn btn-primary my-2" type="submit">Valider</button>
+                </div>
+                
+            </form>
         </div>
-        
-        <div class=" d-flex flex-column justify-content-center <?=((isset($_SESSION['pendu']['game']['tentatives']) && ($_SESSION['pendu']['game']['tentatives']>3))) ? "" : "d-none" ?>">
-            <h4 class="mx-auto">Mot proposées</h4>
+
+        <div class="d-flex justify-content-evenly">
+            <div class=" d-flex flex-column justify-content-center <?=((isset($_SESSION['pendu']['game']['tentatives']))) ? "" : "d-none" ?>">
+                <h4 class="mx-auto">Lettres proposées</h4>
                 <div class="d-flex justify-content-center my-3 gap-3">
                 
-                    <?php if ((isset($_SESSION['pendu']['game']['try_words'])) && (!empty($_SESSION['pendu']['game']['try_words'])))  :?>
-                        <?php foreach ($_SESSION['pendu']['game']['try_words'] as $key => $value) :?> 
+                    <?php if ((isset($_SESSION['pendu']['game']['all_letter_proposed_without_spe_char'])) && (!empty($_SESSION['pendu']['game']['all_letter_proposed_without_spe_char'])))  :?>
+                        <?php foreach ($_SESSION['pendu']['game']['all_letter_proposed_without_spe_char'] as $key => $value) :?> 
                         
-                            <div class=" my-2 text-capitalize border rounded border-dark rounded text-center <?=(($value==$_SESSION['pendu']['game']['word_to_guess_string'])) ? "bg-success" . " " . "text-white" : "bg-danger" . " " . "text-white"?>">
+                            <div class="text-capitalize border rounded border-dark rounded text-center <?=(in_array($value,$_SESSION['pendu']['game']['word_split_without_spe_char'])) ? "bg-success" . " " . "text-white" : ""?>" style="width:20px">
                                 <?=$value?>
                             </div>
                         
                         <?php endforeach?>
                     <?php endif?>
-                </div>         
+                </div>
+            </div>
+            
+            <div class=" d-flex flex-column justify-content-center <?=((isset($_SESSION['pendu']['game']['tentatives'])) && ($_SESSION['pendu']['game']['tentatives']>3) && (isset($_SESSION['pendu']['game']['try_words'])) && (!empty($_SESSION['pendu']['game']['try_words']))) ? "" : "d-none" ?>">
+                <h4 class="mx-auto">Mot proposées</h4>
+                    <div class="d-flex justify-content-center my-3 gap-3">
+                    
+                        <?php if ((isset($_SESSION['pendu']['game']['try_words'])) && (!empty($_SESSION['pendu']['game']['try_words'])))  :?>
+                            <?php foreach ($_SESSION['pendu']['game']['try_words'] as $key => $value) :?> 
+                            
+                                <div class=" my-2 text-capitalize border rounded border-dark rounded text-center <?=(($value==$_SESSION['pendu']['game']['word_to_guess_string'])) ? "bg-success" . " " . "text-white" : "bg-danger" . " " . "text-white"?>">
+                                    <?=$value?>
+                                </div>
+                            
+                            <?php endforeach?>
+                        <?php endif?>
+                    </div>         
+                </div>
             </div>
         </div>
 
--- Portfolio/pendu/traitement-pendu.php
+++ Portfolio/pendu/traitement-pendu.php
@@ -3,7 +3,7 @@ session_start();
 ?>
 <pre><?=var_dump($_SESSION)?></pre>;
 <?php
-CONST MAX_TRY=7;
+CONST MAX_TRY=11;
 
 function all_words_query () {
     try {
@@ -67,6 +67,8 @@ var_dump($_POST);
 
 if ((isset($_POST['launch_game'])) && (!empty($_POST['launch_game']))) {
 
+    // cette condition permet de lancer la partie et va permettre l'affichage des inputs de saisie des lettres.
+
     echo " étape 1 ";
 
     $_SESSION['pendu']['game']['launch_game']=$_POST['launch_game'];
@@ -76,6 +78,10 @@ if ((isset($_POST['launch_game'])) && (!empty($_POST['launch_game']))) {
 
 if ((!isset($_SESSION['pendu']['game']['word'])) && (empty($_SESSION['pendu']['game']['word']))) {
 
+    // Cette condition va permettre de générer un mot aléatoire en le piochant depuis la base de donnée.
+    // Un traitement sera fait sur le mot afin de le stocker tel quel mais aussi de le transformer sans caractères spéciaux pour permettre les comparaisons avec le mot de l'utilisateur ou les lettres.
+    // Un split du mot sera fait pour dynamiser l'affichage et permettre une comparaison lettres par lettres avec celles que soumet l'utilisateur.
+
     echo " étape 2 ";
     $word_to_guess=random_word();
 
@@ -83,25 +89,25 @@ if ((!isset($_SESSION['pendu']['game']['word'])) && (empty($_SESSION['pendu']['g
 
     $_SESSION['pendu']['game']['word_to_guess_split']=mb_str_split($word_to_guess,$length = 1, $encoding = null);
 
+    // La variable  $_SESSION['pendu']['game']['word'] stockera à son index 0 et 1 le mot à deviner sous ses deux formes (array and string).
+
     $_SESSION['pendu']['game']['word'][]=$_SESSION['pendu']['game']['word_to_guess_split'];
 
     $_SESSION['pendu']['game']['word'][]=str_split($_SESSION['pendu']['game']['word_to_guess_without_special_char']);
 
-    $word_without_spe_char=special_char_remplacement($word_to_guess);
+    // la variable $_SESSION['pendu']['game']['word_to_guess_string'] permettra une comparaison entre le mot de l'utilisateur et le mot recherché.
 
-    $_SESSION['pendu']['game']['word_to_guess_string']=str_ireplace("-", "",$word_without_spe_char);
+    $_SESSION['pendu']['game']['word_to_guess_string']=str_ireplace("-", "",$_SESSION['pendu']['game']['word_to_guess_without_special_char']);
 
     $_SESSION['pendu']['game']['word_to_guess_string']=str_ireplace(" ", "",$_SESSION['pendu']['game']['word_to_guess_string']);
 
-
-    
-
-    $_SESSION['pendu']['game']['word_split_without_spe_char']=mb_str_split($word_without_spe_char,$length = 1, $encoding = null);
-
+    $_SESSION['pendu']['game']['word_split_without_spe_char']=mb_str_split($_SESSION['pendu']['game']['word_to_guess_without_special_char'],$length = 1, $encoding = null);
 
 }
 
 if ((isset($_POST['player_letter'])) && (!empty($_POST['player_letter'])) || (isset($_POST['player_word'])) && (!empty($_POST['player_word'])) ) {
+
+    // Cette condition permet d'accéder au coeur du programme. Et évite que les éléments se lancent si $_POST ne comporte rien.
 
     if (!isset($_SESSION['pendu']['game']['tentatives']) && (empty($_SESSION['pendu']['game']['tentatives']))) {
         $_SESSION['pendu']['game']['tentatives']=0;
@@ -109,7 +115,6 @@ if ((isset($_POST['player_letter'])) && (!empty($_POST['player_letter'])) || (is
 
     echo " étape 3 ";
 
-    
     $_POST['player_letter']=strtolower($_POST['player_letter']);
 
     echo " étape 3.1 ";
@@ -124,39 +129,47 @@ if ((isset($_POST['player_letter'])) && (!empty($_POST['player_letter'])) || (is
     (!is_numeric($_POST['player_letter'])) &&
     (strlen($_POST['player_letter']))==1)) {
 
+        // Cette condition permet de ne rentrer que si l'élément proposé ne contient qu'une seule lettre (vérification si l'utilisateur insèrerait plusieurs lettres dans le formulaire ou se tromperait).
+        // Ici il y va avoir le coeur du traitement de l'input soumis par l'utilisateur. 
+
         echo " étape 4 ";
 
 
         if ((isset($_SESSION['pendu']['game']['all_letter_proposed_without_spe_char'])) && (in_array($_POST['player_letter'], $_SESSION['pendu']['game']['all_letter_proposed_without_spe_char']))) {
 
+            // Cette condition permet de vérifier si la lettre n'a pas déjà été proposée, si oui, aucune tentative n'est ajoutée.
+
+
             $_SESSION['pendu']['game']['error']='Lettre déjà proposée';
+
 
         }
         elseif ($_SESSION['pendu']['game']['tentatives']<MAX_TRY) {
 
+            // Si la lettre n'a pas été déjà proposée, alors on rentre dans cette condition
+
             $_SESSION['pendu']['game']['all_letter_proposed_without_spe_char'][]=special_char_remplacement($_POST['player_letter']);
             
-            var_dump($_SESSION['pendu']['game']['all_letter_proposed_without_spe_char']);
             
             echo " étape 4.5 ";
-
-            var_dump($_POST);
-
-          
+         
             
-            if ((in_array($_POST['player_letter'], $_SESSION['pendu']['game']['word'][1])) && ($_SESSION['pendu']['game']['tentatives']<=MAX_TRY)) {
+            if ((in_array($_POST['player_letter'], $_SESSION['pendu']['game']['word'][1])) && ($_SESSION['pendu']['game']['tentatives']<MAX_TRY)) {
+
+                // Si la lettre est contenue dans le tableau $_SESSION['pendu']['game']['word'][1] (qui contient un tableau avec chacune des lettres isolées) et si le max de tentatives n'est pas dépassé
 
                 echo " étape 5 ";
 
                 $_SESSION['pendu']['game']['letter_guessed'][]=$_POST['player_letter'];
-
-                // $_SESSION['pendu']['game']['letter_guessed_string'].=$_POST['player_letter'];
                 
-                $_SESSION['pendu']['game']['tentatives']++;
+                // $_SESSION['pendu']['game']['tentatives']++;
 
                 echo " étape 5.1 ";
 
                 if (isset($_SESSION['pendu']['game']['letter_guessed'])) {
+
+                    // Ici on va boucler sur chacune des lettres du mot sans les caractères spéciaux afin de vérifier si toutes sont comprises dans les lettres devinées par l'utilisateur alors c'est bon. 
+                    // Sinon la variable $result sera set. Ce qui empêchera de dire que la partie est remportée.  
                     
                     foreach ($_SESSION['pendu']['game']['word_split_without_spe_char'] as $value) {
 
@@ -170,6 +183,9 @@ if ((isset($_POST['player_letter'])) && (!empty($_POST['player_letter'])) || (is
 
                     if ((!isset($result)) && ($result!=1)) {
 
+                        // Le joueur ne peut gagner que si toutes les lettres du mot sont comprises dans les lettres qu'il a deviné.
+                        // Chaque victoire se rajoute dans $_SESSION['pendu']['win'] qui ne sera pas remis à 0 lorsqu'une partie est relancée
+
                         $_SESSION['pendu']['game']['result']='Vous avez gagné';
                         $_SESSION['pendu']['win']++;
 
@@ -180,100 +196,86 @@ if ((isset($_POST['player_letter'])) && (!empty($_POST['player_letter'])) || (is
                 unset($_SESSION['pendu']['game']['error']);
                 
                 echo " étape 5.3 ";
-
-                // header('Location: interface-pendu.php');
-                // exit;
+                
             }
 
             elseif ($_SESSION['pendu']['game']['tentatives']<MAX_TRY){
                 
-                $_SESSION['pendu']['game']['tentatives']++;
                 
                 echo " étape 6 ";
 
                 $_SESSION['pendu']['game']['error']="La lettre n'est pas dans le mot";
 
-                // header('Location: interface-pendu.php');
-                // exit;
+                $_SESSION['pendu']['game']['tentatives']++;
+
             }
             
         }else{
+
+             // Chaque défaite se rajoute dans $_SESSION['pendu']['loose'] qui ne sera pas remis à 0 lorsqu'une partie est relancée
 
             echo " étape 7 ";
 
             $_SESSION['pendu']['game']['all_letter_proposed_without_spe_char'][]=special_char_remplacement($_POST['player_letter']);
 
-            $_SESSION['pendu']['game']['result']='Vous avez perdu';
-
-            $_SESSION['pendu']['loose']++;
-
-            // header('Location: interface-pendu.php');
-            // exit;
         }
     }
-    elseif (((strlen($_POST['player_word']))>3) && (isset($_POST['player_word'])) && (!empty($_POST['player_word']))) {
+    elseif (((strlen($_POST['player_word']))>2) && (isset($_POST['player_word'])) && (!empty($_POST['player_word']))) {
+
+        // La proposition de mots doit faire minimum 4 caractères afin de composer un mot complet à deviner. 
+        // Un traitement va être réalisé sur l'input de l'utilisateur afin de retirer les caractères spéciaux et autres éléments pour la comparaison. 
         
         
         $_player_word=special_char_remplacement($_POST['player_word']);
         $_player_word=str_ireplace("-", "", $_player_word);
         $_SESSION['pendu']['game']['player_word']=str_ireplace(" ", "", $_player_word);
+          
+        echo " étape 8 "; 
 
-        if ($_SESSION['pendu']['game']['tentatives']>MAX_TRY) {
 
-            echo " étape 8 ";
+        if ($_SESSION['pendu']['game']['word_to_guess_string']==$_SESSION['pendu']['game']['player_word']) {
+
+            echo " étape 9 "; 
+
+            $_SESSION['pendu']['game']['result']='Vous avez gagné';
+            $_SESSION['pendu']['game']['try_words'][]=$_SESSION['pendu']['game']['player_word'];
+            $_SESSION['pendu']['win']++;
+            
+
+        }elseif ((isset($_SESSION['pendu']['game']['try_words'])) && (in_array($_SESSION['pendu']['game']['player_word'],$_SESSION['pendu']['game']['try_words']))) {
+
+            $_SESSION['pendu']['game']['error']="Mot déjà proposé";
+
+        }
+        else {
+
+            echo " étape 10 "; 
 
             $_SESSION['pendu']['game']['try_words'][]=$_SESSION['pendu']['game']['player_word'];
 
-            $_SESSION['pendu']['game']['result']='Vous avez perdu';
-
-            $_SESSION['pendu']['loose']++;
-
-        }else {
-            $_SESSION['pendu']['game']['error']="Ce n'est pas le bon mot";
-            echo " étape 9 "; 
             $_SESSION['pendu']['game']['tentatives']++;
 
+            $_SESSION['pendu']['game']['error']="Ce n'est pas le bon mot";
 
-            if ($_SESSION['pendu']['game']['word_to_guess_string']==$_SESSION['pendu']['game']['player_word']) {
-
-                echo " étape 10 "; 
-
-                $_SESSION['pendu']['game']['result']='Vous avez gagné';
-                $_SESSION['pendu']['game']['try_words'][]=$_SESSION['pendu']['game']['player_word'];
-                $_SESSION['pendu']['win']++;
-                // header('Location: interface-pendu.php');
-                // exit;
-
-            }elseif ((isset($_SESSION['pendu']['game']['try_words'])) && (in_array($_SESSION['pendu']['game']['player_word'],$_SESSION['pendu']['game']['try_words']))) {
-
-                $_SESSION['pendu']['game']['error']="Mot déjà proposé";
-
-            }
-            else {
-
-                echo " étape 11 "; 
-
-                $_SESSION['pendu']['game']['try_words'][]=$_SESSION['pendu']['game']['player_word'];
-
-                $_SESSION['pendu']['game']['tentatives']++;
-                    
-            
-
-                $_SESSION['pendu']['game']['error']="Ce n'est pas le bon mot";
-                // header('Location: interface-pendu.php');
-                // exit;
-
-            }
         }
+        
 
     }
     else{
-        echo " étape 12 ";
+        echo " étape 11 ";
         $_SESSION['pendu']['game']['error']="saisie incorrecte";
     }
 }
 
-echo " étape 13 ";
+if ((isset($_SESSION['pendu']['game']['tentatives'])) && ($_SESSION['pendu']['game']['tentatives']>=MAX_TRY)) {
+
+    $_SESSION['pendu']['game']['result']='Vous avez perdu';
+
+    $_SESSION['pendu']['loose']++;
+    
+}
+
+echo " étape 12 ";
 header('Location: interface-pendu.php');
 exit;
 
